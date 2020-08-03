8b2ae479c41719ca4985dea1b575fb9ef879ada9
theirishkiwi <theirishkiwi@gmail.com>
2020-08-03 10:21:00 +0100

    Copright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - 2020</p>
+        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - © 2020</p>
     </footer>
 )
 
