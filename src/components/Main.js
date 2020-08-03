9af4e7ac59c72a9fb313401eee25853f0b18308b
theirishkiwi <theirishkiwi@gmail.com>
2020-08-03 21:04:27 +0100

    Netlify form

--- src/components/Main.js
+++ src/components/Main.js
@@ -115,7 +115,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action="#" netlify>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
