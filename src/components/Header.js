19fc1b22d84092891ad6185e8c0ec2aaf260d22f
theirishkiwi <theirishkiwi@gmail.com>
2020-08-04 17:02:10 +0100

    Png

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import logo from '../images/logo.jpg'
+import logo from '../images/logo.png'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
