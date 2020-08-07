811da233ff1b542df3c3668486d78cc08d3b1f1d
theirishkiwi <theirishkiwi@gmail.com>
2020-08-07 21:13:18 +0100

    move insta to a new para

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - 2020 - <a href="https://www.instagram.com/pickedorganic/">Lets be social! <span className="icon fa-instagram"></span></a></p>
+        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - 2020</p>
+        <p className="copyright"><a href="https://www.instagram.com/pickedorganic/"><span className="icon fa-instagram"></span></a></p>
     </footer>
 )
 
