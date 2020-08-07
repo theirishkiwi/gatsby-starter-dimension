f4f5aa8de43c69aabf28e1a02ff84940b5947aaf
theirishkiwi <theirishkiwi@gmail.com>
2020-08-07 21:07:12 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - 2020 - <a href="https://www.instagram.com/pickedorganic/">Lets be social!<span className="icon fa-instagram"></span></a></p>
+        <p className="copyright">&copy; Picked Organic- Farming using organic princiles - 2020 - <a href="https://www.instagram.com/pickedorganic/">Lets be social! <span className="icon fa-instagram"></span></a></p>
     </footer>
 )
 
