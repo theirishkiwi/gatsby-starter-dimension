691b6c677280e2e1021286586863b1a9e7405d3e
theirishkiwi <theirishkiwi@gmail.com>
2020-08-19 22:09:34 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -43,8 +43,10 @@ We have the following options for subscriptions: weekly, fortnightly and monthly
 <p><input type="text" placeholder="First Name" name="first_name" required /></p>
 <p><input type="email" placeholder="Email" name="email" required /></p>
 <p><label><input type="checkbox" name="gdpr" value="1" required /> I agree to receive email updates and promotions.</label></p>
+
 {/* -- no botz please */}
-<div style={{position: absolute, left: '-5000px',}} aria-hidden="true"><input type="text" name="a_password" tabindex="-1" value="" autocomplete="off" /></div>
+
+<div style={{position: 'absolute', left: '-5000px',}} aria-hidden="true"><input type="text" name="a_password" tabindex="-1" value="" autocomplete="off" /></div>
 <p><button type="submit">Subscribe!</button></p>
 </form>
 <script src="https://sendfox.com/js/form.js"></script>	
